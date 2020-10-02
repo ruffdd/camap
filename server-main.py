21b072a1fad50e2454f9d6f20df1002939094cf5
ruffdd <david.ruff@live.de>
2020-10-02 13:30:38 +0200

    add leaflet

--- server-main.py
+++ server-main.py
@@ -1,7 +1,10 @@
-from flask import Flask,render_template,send_file
+from flask import Flask,render_template,send_file,send_from_directory
 app = Flask(__name__,static_url_path='/static')
 
 @app.route('/', methods=['GET'])
 def root():
     return send_file('./static/index.html')
 
+@app.route('/leaflet/<path:path>', methods=['GET'])
+def leaflet(path):
+    return send_from_directory('libs/leaflet',path)
