41957084ce550a2785031423585c5e8e8249fc47
ruffdd <david.ruff@live.de>
2020-10-08 16:20:04 +0200

    add add building

--- db.py
+++ db.py
@@ -1,5 +1,7 @@
 import mysql.connector
-
+import os
+from flask import Flask
+flask_app: Flask
 mydb = mysql.connector.connect(
   host="localhost",
   database="camap",
@@ -10,9 +12,46 @@ mydb = mysql.connector.connect(
 cursor = mydb.cursor()
 
 def tables():
-    cursor.execute("show tables;")
-    return cursor.fetchall()
+  cursor.execute("show tables;")
+  return cursor.fetchall()
 
-if len(tables()) == 0 :
-    cursor.execute("CREATE TABLE buildings (id VARCHAR(20) PRIMARY KEY);")
+def get_building(osm_id:int):
+  result={}
+  result['id']=osm_id
+  result['name']=""
+  result['description_url']=""
+  result['adress_name']=""
+  return result
+
+def update_building(input:dict):
+  cursor.execute("INSERT INTO OSMBuildings (id,adress,descript) VALUES ("+input['id']+','+input['adress_name']+','+input['description_url']+');')
+
+def load_sql_file(name):
+  path="./sql/"+ name +("" if name.endswith(".sql") else ".sql")
+  file = open(path)
+  commands=[]
+  for line in file.read().split(";"):
+    commands.append(line+";")
+  #text = text.replace("\n","").strip().replace(";",";\n")
+  return commands
+
+def setup(app:Flask):
+  global flask_app
+  flask_app=app
+  sql_file_name=""
+  if len(tables()) == 0 :
+      sql_file_name="setup"
+  if(sql_file_name!=""):
+    try:
+      sql_file = load_sql_file(sql_file_name)
+      for command in sql_file:
+        flask_app.logger.debug("executing: "+command)
+        cursor.execute(command)
+      response = cursor.fetchall()
+      flask_app.logger.info(str(response))
+
+      flask_app.logger.info("created oder updated database")
+    except FileNotFoundError:
+      flask_app.logger.error('could no load sql file "' + sql_file_name+'" to initialize database')
+
 
--- server-main.py
+++ server-main.py
@@ -1,7 +1,13 @@
 from flask import Flask,render_template,send_file,send_from_directory
+from os import getenv
 import db
 app = Flask(__name__,static_url_path='/static')
-
+development= getenv("FLASK_ENV")=="development"
+if development:
+    app.logger.setLevel(10)
+else:
+    app.logger.setLevel(30)
+db.setup(app)
 
 
 @app.route('/', methods=['GET'])
@@ -12,7 +18,12 @@ def root():
 def leaflet(path):
     return send_from_directory('libs/leaflet',path)
 
-@app.route('/deb/mysql/print',methods=['GET'])
-def debug():
-    text=str(db.tables()).replace('(','').replace(')','').replace('[','').replace(']','').replace("'",'').replace(',',"<br>")
-    return text
+@app.route('/api/building/r<int:osm_id>',methods=['GET'])
+def get_building(osm_id):
+    return db.get_building(osm_id)
+
+if development:
+    @app.route('/deb/mysql/print',methods=['GET'])
+    def debug():
+        text=str(db.tables()).replace('(','').replace(')','').replace('[','').replace(']','').replace("'",'').replace(',',"<br>")
+        return text
